6b403bca5a61688a523eaf0be0a0d282f29ba5cd
Tatijana3004 <tatijana3004@mail.ru>
2022-04-22 19:47:49 +0300

    ДЗ_8
    ДЗ_8

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,6 @@
 export const Home = ({ onAuth }) => (
     <>
-        <h4>Home page</h4>;
+        <h4>Home page</h4>
         <button onClick={onAuth}>Auth</button>
     </>
 );
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -4,9 +4,9 @@ import { setName, toggleCheckbox } from "../../store/profile/actions";
 import { selectName, selectShowName } from "../../store/profile/selectors";
 import { usePrev } from "../../utils/usePrev";
 
-// Использованы хуки
 export const Profile = ({ onLogout }) => {
     const dispatch = useDispatch();
+
     const name = useSelector(selectName);
     const showName = useSelector(selectShowName);
     const handleClick = () => {
@@ -18,7 +18,8 @@ export const Profile = ({ onLogout }) => {
 
     const handleSubmit = (text) => {
         dispatch(setName(text));
-    }
+    };
+
     return (
         <>
             <h3>This is Profile {showName && <span>{name}</span>}</h3>
@@ -34,41 +35,3 @@ export const Profile = ({ onLogout }) => {
     );
 };
 
-// Использован коннект
-// export const ProfileToConnect = ({ name, showName, changeName, changeCheckbox }) => {
-//     const handleClick = () => {
-//         changeCheckbox();
-//     };
-
-//     const handleSubmit = (text) => {
-//         changeName(text);
-//     };
-//     return (
-//         <>
-//             <h3>This is Profile
-//                 {showName && <span>{name}</span>}
-//             </h3>
-//             <Form onSubmit={handleSubmit} />
-
-//             <div>
-//                 <input type="checkbox" id="checkbox" onClick={handleClick}></input>
-//                 <label for="checkbox">change show name</label>
-//             </div>
-//         </>
-//     );
-// };
-
-// const mapStateToProps = state => ({
-//     name: state.profile.name,
-//     showName: state.profile.showName,
-// });
-
-// const mapDispatchToProps = {
-//     changeName: setName,
-//     changeCheckbox: () => toggleCheckbox,
-// }
-
-// export default Profile = connect(
-//     mapStateToProps, mapDispatchToProps
-// )
-//     (ProfileToConnect);
